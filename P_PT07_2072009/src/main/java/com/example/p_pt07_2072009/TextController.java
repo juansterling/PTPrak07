a0918c31a59bdbfadef2239d0e3708a37debb0ab
juansterling <2072009@maranatha.ac.id>
2022-08-09 18:30:22 +0700

    PTPrak07

---
+++ P_PT07_2072009/src/main/java/com/example/p_pt07_2072009/TextController.java
@@ -0,0 +1,62 @@
+package com.example.p_pt07_2072009;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TextController {
+    @FXML
+    private TextArea txt;
+    private Path path;
+
+    public void open(){
+        FileChooser chooser = new FileChooser();
+        FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("Text Documents", "*.txt");
+        chooser.getExtensionFilters().add(extensionFilter);
+        chooser.setSelectedExtensionFilter(extensionFilter);
+        File f = chooser.showOpenDialog(txt.getScene().getWindow());
+        if (f != null) {
+            Path p = Paths.get(f.toURI());
+            try {
+                String jsonString = Files.readString(p);
+                txt.setText(jsonString);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void save(){
+        if (path!= null) {
+            try {
+                Files.write(path, txt.getText().getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            saveas();
+        }
+    }
+    public void saveas(){
+        FileChooser chooser = new FileChooser();
+        FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("Text Documents", "*.txt");
+        chooser.getExtensionFilters().add(extensionFilter);
+        chooser.setSelectedExtensionFilter(extensionFilter);
+        File f = chooser.showSaveDialog(txt.getScene().getWindow());
+        if (f != null) {
+            path = Paths.get(f.toURI());
+            try {
+                Files.write(path, txt.getText().getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
---
+++ P_PT07_2072009/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.p_pt07_2072009 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.p_pt07_2072009 to javafx.fxml;
+    exports com.example.p_pt07_2072009;
+}
